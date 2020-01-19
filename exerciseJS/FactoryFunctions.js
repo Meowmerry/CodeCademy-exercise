c4790d4e6936b0a0de7c9841981f44a083d61fad
meowmerry <meow3525@gmail.com>
2020-01-19 21:25:36 +0700

    [added] add more exercise

---
+++ exerciseJS/FactoryFunctions.js
@@ -0,0 +1,42 @@
+const monsterFactory = (name, age, energySource, catchPhrase) => {
+  return {
+    name: name,
+    age: age,
+    energySource: energySource,
+    scare() {
+      console.log(catchPhrase);
+    }
+  };
+};
+const ghost = monsterFactory("Ghouly", 251, "ectoplasm", "BOO!");
+ghost.scare(); // 'BOO!'
+
+//====================================================
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    }
+  };
+};
+
+const tinCan = robotFactory("P-500", true);
+tinCan.beep(); //Beep Boop
+
+//===============================
+function robotFactory(model, mobile){
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
