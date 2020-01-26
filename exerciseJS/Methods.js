ef9da503fa9fa11b63f49b50db33cffd0c0ba2e5
meowmerry <meow3525@gmail.com>
2020-01-27 01:20:20 +0700

    [added] class Methods.js

--- exerciseJS/Methods.js
+++ exerciseJS/Methods.js
@@ -4,3 +4,35 @@ const alienShip = {
     }
   };
   alienShip.invade();
+
+  //===============================
+  class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+surgeonCurry.takeVacationDays(14); // 6
+console.log(surgeonCurry.remainingVacationDays);  
+console.log(surgeonDurant);
+ 
