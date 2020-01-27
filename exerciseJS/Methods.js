84a68ee751c66108be65982ce843695ea1946437
meowmerry <meow3525@gmail.com>
2020-01-27 15:34:41 +0700

    added

--- exerciseJS/Methods.js
+++ exerciseJS/Methods.js
@@ -32,7 +32,36 @@ const alienShip = {
 
 const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
 const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
 surgeonCurry.takeVacationDays(14); // 6
 console.log(surgeonCurry.remainingVacationDays);  
-console.log(surgeonDurant);
- 
+console.log(surgeonDurant.remainingVacationDays);
+
+
+ //======================================
+ class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get behavior() {
+    return this._behavior;
+  }   
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+const halley = new Dog('Halley');
+console.log(halley.incrementBehavior());
+
+let nikko = new Dog('Nikko'); // Create dog named Nikko
+nikko.incrementBehavior(); // Add 1 to nikko instance's behavior
+
+let bradford = new Dog('Bradford'); // Creat dog name Bradford and
