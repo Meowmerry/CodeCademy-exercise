4be41cf6c0a6110560079c50e8036877b11f1271
meowmerry <meow3525@gmail.com>
2020-02-02 20:13:41 +0700

    [added]

---
+++ exerciseJS/TruthyAndFalsy.js
@@ -0,0 +1,29 @@
+let wordCount = 2;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+// Great! You've started your work!
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+//This string is definitely empty.
+
+
+
+//Truthy and Falsy Assignment
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen'
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+//The marker is mightier than the sword.
