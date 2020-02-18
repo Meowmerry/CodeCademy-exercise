789a8afca40212ff2b863b235a1f4ec19a7eb5f9
meowmerry <meow3525@gmail.com>
2020-02-18 08:54:52 +0700

    [added] array

---
+++ exerciseJS/FromLesson/Arrays.js
@@ -0,0 +1,12 @@
+//Arrays
+
+const hobbies = ['swiming', 'basgetball','reading', 'coding'];
+console.log(hobbies) //[ 'swiming', 'basgetball', 'reading', 'coding' ]
+
+//===========================================================================
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0]; 
+console.log(listItem) //Fortune favors the brave.
+console.log(famousSayings[2]); //Where there is love there is life.
+console.log(famousSayings[3]); //undefined
