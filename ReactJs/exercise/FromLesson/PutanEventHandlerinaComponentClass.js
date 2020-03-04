2f669f4a34f21481a036ead025c95216d3064830
meowmerry <meow3525@gmail.com>
2020-03-04 22:57:44 +0700

    added

---
+++ ReactJs/exercise/FromLesson/PutanEventHandlerinaComponentClass.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class Example extends React.Component {
+  handleEvent() {
+    alert(`I am an event handler.
+      If you see this message,
+      then I have been called.`);
+  }
+
+  render() {
+    return (
+      <h1 onClick={this.handleEvent}>
+        Hello world
+      </h1>
+    );
+  }
+}
+
+//=======================================
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './Button';
+
+
+
+class Talker extends React.Component {
+  talk () {
+	let speech = '';
+  for (let i = 0; i < 10000; i++) {
+    speech += 'blah ';
+  }
+  alert(speech);
+}
+  render() {
+    return <Button />;
+  }
+}
+
+ReactDOM.render(
+  <Talker />,
+  document.getElementById('app')
+);
+
+//===============================
+import React from 'react';
+
+export class Button extends React.Component {
+  render() {
+    return (
+      <button>
+        Click me!
+      </button>
+    );
+  }
+}
