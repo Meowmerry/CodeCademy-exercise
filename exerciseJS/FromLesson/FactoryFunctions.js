6cf7c94a9bf572eb3ce410cd62033226f64d82c0
meowmerry <meow3525@gmail.com>
2020-08-10 23:52:18 +0700

    js

--- exerciseJS/FromLesson/FactoryFunctions.js
+++ exerciseJS/FromLesson/FactoryFunctions.js
@@ -40,3 +40,19 @@ function robotFactory(model, mobile){
 const newRobot = robotFactory('P-501', false)
 console.log(newRobot.model)
 console.log(newRobot.mobile)
+
+// ---------------------------  Property Value Shorthand --------------------------- 
+function robotFactory(model, mobile){
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
