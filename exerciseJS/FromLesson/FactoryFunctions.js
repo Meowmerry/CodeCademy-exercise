76650817780508536414f526ddaafea340a43057
meowmerry <meow3525@gmail.com>
2020-08-14 22:50:34 +0700

    added

--- exerciseJS/FromLesson/FactoryFunctions.js
+++ exerciseJS/FromLesson/FactoryFunctions.js
@@ -56,3 +56,45 @@ function robotFactory(model, mobile){
 const newRobot = robotFactory('P-501', false)
 console.log(newRobot.model)
 console.log(newRobot.mobile)
+
+// ------------  Destructured Assignment  ------------ 
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+const {functionality } = robot;
+functionality.beep();
+
+
+// ------------  Built-in Object Methods  ------------ 
+
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot)
+
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true})
+
+console.log(newRobot);
