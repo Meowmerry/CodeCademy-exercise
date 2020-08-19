723592117d33146ccf7343eaff3c0aa78036555a
meowmerry <meow3525@gmail.com>
2020-08-19 19:14:02 +0700

    find my hate

--- exerciseJS/FromLesson/ClassesMethods.js
+++ exerciseJS/FromLesson/ClassesMethods.js
@@ -17,3 +17,98 @@ class Dog {
       this._behavior++;
     }
   }
+
+  //------------------- Methods  ------------------------------------------------------------------
+  class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+   
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+console.log(surgeonCurry.name)
+
+surgeonCurry.takeVacationDays(3)
+console.log(surgeonCurry.remainingVacationDays) // 17
+
+
+
+
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+  static generatePassword(){
+    const passwordRandom = Math.floor(Math.random() *1000) ;
+    return passwordRandom;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  } 
+  get certifications(){
+    return this._certifications;
+  }
+  addCertification(newCertification){
+    this._certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk',[ 'Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
